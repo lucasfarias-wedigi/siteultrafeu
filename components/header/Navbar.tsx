b405ae256577976dd0da610e1b9eaf810ca739ad
gabriel branco <gabriel.branco@wedigi.com.br>
2024-06-21 18:44:06 -0300

    lint

--- components/header/Navbar.tsx
+++ components/header/Navbar.tsx
@@ -36,7 +36,7 @@ function Navbar({
 }) {
   const platform = usePlatform();
   const liveStoreItem = benefitsItems.find(
-    (item) => item.text.toLowerCase() === "loja ao vivo"
+    (item) => item.text.toLowerCase() === "loja ao vivo",
   );
   const liveStoreText = liveStoreItem?.text;
   const liveStoreLink = liveStoreItem?.link;
@@ -94,17 +94,21 @@ function Navbar({
             </a>
           )}
         </div>
-        {/* <ul class={`flex gap-6 col-span-1 justify-start`}>
+        {
+          /* <ul class={`flex gap-6 col-span-1 justify-start`}>
         {items.map((item) => (
           <NavItem item={item} />
         ))}
-      </ul> */}
+      </ul> */
+        }
         <div class="flex-none flex items-center justify-end gap-6 col-span-5">
-          {/* {!buttons?.hideSearchButton && (
+          {
+            /* {!buttons?.hideSearchButton && (
           <div class="flex items-center text-xs font-thin gap-1">
             <SearchButton />
           </div>
-        )} */}
+        )} */
+          }
           <VisibilityOnScroll handleShow="inverse">
             <button class="flex items-center mr-20 w-full">
               <Icon id="MenuHamburguer" strokeWidth={1} size={44} />
@@ -138,11 +142,13 @@ function Navbar({
               <button class="text-blueSecondary">
                 {platform === "vtex" && <CartButtonVTEX />}
               </button>
-              {/* {platform === "vnda" && <CartButtonVDNA />}
+              {
+                /* {platform === "vnda" && <CartButtonVDNA />}
               {platform === "wake" && <CartButtonWake />}
               {platform === "linx" && <CartButtonLinx />}
               {platform === "shopify" && <CartButtonShopify />}
-              {platform === "nuvemshop" && <CartButtonNuvemshop />} */}
+              {platform === "nuvemshop" && <CartButtonNuvemshop />} */
+              }
             </div>
           )}
         </div>
