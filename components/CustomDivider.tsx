09026874878ab3e5bff7e6326253d98b2b10f979
gabriel branco <gabriel.branco@wedigi.com.br>
2024-06-27 16:52:29 -0300

    ajuste customdivider

--- components/CustomDivider.tsx
+++ components/CustomDivider.tsx
@@ -4,9 +4,9 @@ interface Props {
   children: ComponentChildren;
 }
 
-const SectionDivider = ({ children }: Props) => {
+const CustomDivider = ({ children }: Props) => {
   return (
-    <div class="flex items-center text-center">
+    <div class="w-full flex items-center text-center">
       <div class="w-full bg-[#E2E2E2] h-[1px] mr-4" />
       {children}
       <div class="w-full bg-[#E2E2E2] h-[1px] ml-4" />
@@ -14,4 +14,4 @@ const SectionDivider = ({ children }: Props) => {
   );
 };
 
-export default SectionDivider;
+export default CustomDivider;
--- sections/ChoiceUltrafeu.tsx
+++ sections/ChoiceUltrafeu.tsx
@@ -1,5 +1,6 @@
 import Image from "apps/website/components/Image.tsx";
 import { ImageWidget } from "apps/admin/widgets.ts";
+import CustomDivider from "../components/CustomDivider.tsx";
 
 /**
  * @titleBy text
@@ -20,9 +21,11 @@ export interface Props {
 const ChoiceUltrafeu = ({ title, items }: Props) => {
   return (
     <div class="w-full max-w-7xl m-auto">
-      <h2 class="text-center text-blackPrimary font-semibold text-2xl">
-        {title}
-      </h2>
+      <CustomDivider>
+        <h2 class="text-center text-blackPrimary font-semibold text-2xl">
+          {title}
+        </h2>
+      </CustomDivider>
       <div class="w-full flex items-center justify-around">
         {items?.map((item) => (
           <div class="w-[152px] text-center">
