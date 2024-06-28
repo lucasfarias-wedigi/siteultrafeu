46d1d2f0a397d04205f6330b9d83d920eb314ac6
gabriel branco <gabriel.branco@wedigi.com.br>
2024-06-28 14:26:10 -0300

    feat: adiciona section Brands

---
+++ sections/Brands.tsx
@@ -0,0 +1,89 @@
+import Image from "apps/website/components/Image.tsx";
+import { ImageWidget } from "apps/admin/widgets.ts";
+import CustomDivider from "../components/CustomDivider.tsx";
+import Slider from "../components/ui/Slider.tsx";
+import Icon from "../components/ui/Icon.tsx";
+import { useId } from "../sdk/useId.ts";
+
+/**
+ * @titleBy text
+ */
+interface item {
+  image: ImageWidget;
+  text: string;
+}
+
+/**
+ * @titleBy title
+ */
+export interface Props {
+  title: string;
+  items: item[];
+}
+
+const Brands = ({ title, items }: Props) => {
+  const id = useId();
+  return (
+    <div id={id} class="w-full max-w-7xl mx-auto relative md:px-0 px-4 mb-8">
+      <CustomDivider>
+        <h2 class="text-start md:text-center text-blackPrimary font-semibold text-2xl whitespace-nowrap">
+          {title}
+        </h2>
+        <div class="flex items-center justify-end gap-2.5 md:hidden">
+          <Slider.PrevButton class="w-11 h-11 rounded-full bg-grayTertiary flex justify-center items-center">
+            <Icon
+              size={18}
+              id="ChevronRight"
+              strokeWidth={3}
+              class="text-purplePrimary rotate-180 w-full"
+            />
+          </Slider.PrevButton>
+          <Slider.NextButton class="w-11 h-11 rounded-full bg-grayTertiary flex justify-center items-center">
+            <Icon
+              size={18}
+              id="ChevronRight"
+              strokeWidth={3}
+              class="text-purplePrimary"
+            />
+          </Slider.NextButton>
+        </div>
+      </CustomDivider>
+
+      {/* <div class="w-full flex items-center justify-around">
+        {items?.map((item) => (
+          <div class="w-[152px] text-center">
+            <Image
+              src={item.image}
+              alt={item.text}
+              width={88}
+              height={66}
+              class="m-auto"
+            />
+            <p class="text-sm text-blackPrimary">{item.text}</p>
+          </div>
+        ))}
+      </div> */}
+
+      <Slider class="carousel w-full justify-between carousel-center">
+        {items?.map((item, index) => (
+          <Slider.Item index={index} class="carousel-item">
+            <div class="w-[100px] text-center">
+              <Image
+                src={item.image}
+                alt={item.text}
+                width={100}
+                height={60}
+                class="m-auto"
+              />
+              <p class="text-sm text-blackPrimary">{item.text}</p>
+            </div>
+          </Slider.Item>
+        ))}
+      </Slider>
+
+      <Slider.JS rootId={id} />
+    </div>
+  );
+};
+
+export default Brands;
