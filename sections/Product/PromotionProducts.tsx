0bb3d3b893fbd668742340d94db18d943b4ef1d7
gabriel branco <gabriel.branco@wedigi.com.br>
2024-07-03 02:10:17 -0300

    lint

--- sections/Product/PromotionProducts.tsx
+++ sections/Product/PromotionProducts.tsx
@@ -33,78 +33,80 @@ function PromotionProducts({
 
   return (
     <div id={id} class="w-full max-w-7xl px-4 lg:px-0 mx-auto mb-8">
-      {device === "desktop" ? (
-        <>
-          <CustomDivider>
-            <h2 class="text-start md:text-center text-blackPrimary text-base lg:text-2xl font-semibold whitespace-nowrap">
-              {title}
-            </h2>
-          </CustomDivider>
-          <div class="w-full flex justify-between">
-            <div class="flex w-full">
-              <PromotionProductCardInHighLight
-                product={products[0]}
-                itemListName={title}
-                // platform={platform}
-                index={0}
-              />
-            </div>
-            <div class="flex flex-wrap justify-end gap-4 items-center w-full h-full">
-              {products &&
-                products.map((product, index) => (
-                  <PromotionProductCard
-                    product={product}
-                    itemListName={title}
-                    // platform={platform}
-                    index={index}
-                  />
-                ))}
-            </div>
-          </div>
-        </>
-      ) : (
-        <>
-          <div class="relative flex items-center w-full">
+      {device === "desktop"
+        ? (
+          <>
             <CustomDivider>
-              <h2 class="text-start md:text-center text-blackPrimary font-semibold text-base lg:text-2xl whitespace-nowrap">
+              <h2 class="text-start md:text-center text-blackPrimary text-base lg:text-2xl font-semibold whitespace-nowrap">
                 {title}
               </h2>
             </CustomDivider>
-            <div class="bg-white absolute z-10 right-0 flex items-center justify-end gap-2.5 mb-8 pl-8">
-              <Slider.PrevButton class="w-11 h-11 rounded-full bg-grayTertiary flex justify-center items-center">
-                <Icon
-                  size={18}
-                  id="ChevronRight"
-                  strokeWidth={3}
-                  class="text-purplePrimary rotate-180 w-full"
-                />
-              </Slider.PrevButton>
-              <Slider.NextButton class="w-11 h-11 rounded-full bg-grayTertiary flex justify-center items-center">
-                <Icon
-                  size={18}
-                  id="ChevronRight"
-                  strokeWidth={3}
-                  class="text-purplePrimary"
-                />
-              </Slider.NextButton>
-            </div>
-          </div>
-          <Slider class="w-full carousel carousel-center">
-            {products?.map((product, index) => (
-              <Slider.Item index={index} class={"carousel-item w-full"}>
+            <div class="w-full flex justify-between">
+              <div class="flex w-full">
                 <PromotionProductCardInHighLight
-                  product={product}
+                  product={products[0]}
                   itemListName={title}
                   // platform={platform}
                   index={0}
                 />
-              </Slider.Item>
-            ))}
-          </Slider>
+              </div>
+              <div class="flex flex-wrap justify-end gap-4 items-center w-full h-full">
+                {products &&
+                  products.map((product, index) => (
+                    <PromotionProductCard
+                      product={product}
+                      itemListName={title}
+                      // platform={platform}
+                      index={index}
+                    />
+                  ))}
+              </div>
+            </div>
+          </>
+        )
+        : (
+          <>
+            <div class="relative flex items-center w-full">
+              <CustomDivider>
+                <h2 class="text-start md:text-center text-blackPrimary font-semibold text-base lg:text-2xl whitespace-nowrap">
+                  {title}
+                </h2>
+              </CustomDivider>
+              <div class="bg-white absolute z-10 right-0 flex items-center justify-end gap-2.5 mb-8 pl-8">
+                <Slider.PrevButton class="w-11 h-11 rounded-full bg-grayTertiary flex justify-center items-center">
+                  <Icon
+                    size={18}
+                    id="ChevronRight"
+                    strokeWidth={3}
+                    class="text-purplePrimary rotate-180 w-full"
+                  />
+                </Slider.PrevButton>
+                <Slider.NextButton class="w-11 h-11 rounded-full bg-grayTertiary flex justify-center items-center">
+                  <Icon
+                    size={18}
+                    id="ChevronRight"
+                    strokeWidth={3}
+                    class="text-purplePrimary"
+                  />
+                </Slider.NextButton>
+              </div>
+            </div>
+            <Slider class="w-full carousel carousel-center">
+              {products?.map((product, index) => (
+                <Slider.Item index={index} class={"carousel-item w-full"}>
+                  <PromotionProductCardInHighLight
+                    product={product}
+                    itemListName={title}
+                    // platform={platform}
+                    index={0}
+                  />
+                </Slider.Item>
+              ))}
+            </Slider>
 
-          <Slider.JS rootId={id} />
-        </>
-      )}
+            <Slider.JS rootId={id} />
+          </>
+        )}
 
       <SendEventOnView
         id={id}
