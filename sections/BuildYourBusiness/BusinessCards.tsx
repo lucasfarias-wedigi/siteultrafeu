76cccfba33b76a32082732c56a766b6959e62dbe
gabriel branco <gabriel.branco@wedigi.com.br>
2024-07-11 14:45:48 -0300

    feat: adiciona link ao card

--- sections/BuildYourBusiness/BusinessCards.tsx
+++ sections/BuildYourBusiness/BusinessCards.tsx
@@ -8,6 +8,7 @@ interface businessCard {
   title: string;
   image: ImageWidget;
   buttonText: string;
+  link: string;
 }
 
 interface Props {
@@ -20,15 +21,22 @@ const BusinessCards = ({ businessCards }: Props) => {
     <div class="flex justify-center lg:justify-start w-full gap-8 flex-wrap my-4 max-w-7xl mx-auto">
       {businessCards.map((item) => (
         <div class="max-w-[163px] lg:max-w-[280px] w-full p-2 rounded-card border border-grayTertiary">
-          <div class="relative w-full mb-2.5">
-            <div class="lg:absolute top-0 bg-purplePrimary w-full text-white font-semibold text-sm flex items-center justify-center py-1">
-              {item.title}
+          <a href={item.link}>
+            <div class="relative w-full mb-2.5">
+              <div class="lg:absolute top-0 bg-purplePrimary w-full text-white font-semibold text-sm flex items-center justify-center py-1">
+                {item.title}
+              </div>
+              <Image
+                src={item.image}
+                alt={item.title}
+                width={264}
+                height={100}
+              />
             </div>
-            <Image src={item.image} alt={item.title} width={264} height={100} />
-          </div>
-          <button class="flex items-center justify-center w-full py-2 rounded-card border border-purplePrimary text-purplePrimary text-base font-bold">
-            {item.buttonText}
-          </button>
+            <button class="flex items-center justify-center w-full py-2 rounded-card border border-purplePrimary text-purplePrimary text-base font-bold">
+              {item.buttonText}
+            </button>
+          </a>
         </div>
       ))}
     </div>
