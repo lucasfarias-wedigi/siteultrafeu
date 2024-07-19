3712675ab6f2a17b835ab36630dec89b7cbbbd66
Paul Oliver <paulo.jr@wedigi.com.br>
2024-07-19 16:37:38 +0000

    Release production environment with 3 changes made by Gabriel Branco, Cleyton Mendes, Paul Oliver

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -30,10 +30,10 @@ import * as $$$$$$7 from "./sections/Cards/IconCardsCarousel.tsx";
 import * as $$$$$$8 from "./sections/Cards/IconCardsCarouselWithImage.tsx";
 import * as $$$$$$9 from "./sections/Cards/ImageCardsCarousel.tsx";
 import * as $$$$$$10 from "./sections/Cards/ImageCardsCarouselWithImage.tsx";
-import * as $$$$$$12 from "./sections/Category/CategoryBanner.tsx";
-import * as $$$$$$13 from "./sections/Category/CategoryGrid.tsx";
-import * as $$$$$$14 from "./sections/Category/CategoryList.tsx";
-import * as $$$$$$11 from "./sections/CategoryList.tsx";
+import * as $$$$$$11 from "./sections/Category/CategoryBanner.tsx";
+import * as $$$$$$12 from "./sections/Category/CategoryGrid.tsx";
+import * as $$$$$$13 from "./sections/Category/CategoryList.tsx";
+import * as $$$$$$14 from "./sections/CategoryList.tsx";
 import * as $$$$$$15 from "./sections/ChoiceUltrafeu.tsx";
 import * as $$$$$$16 from "./sections/Content/Benefits.tsx";
 import * as $$$$$$17 from "./sections/Content/BlogPosts.tsx";
@@ -105,10 +105,10 @@ const manifest = {
     "site/sections/Cards/IconCardsCarouselWithImage.tsx": $$$$$$8,
     "site/sections/Cards/ImageCardsCarousel.tsx": $$$$$$9,
     "site/sections/Cards/ImageCardsCarouselWithImage.tsx": $$$$$$10,
-    "site/sections/Category/CategoryBanner.tsx": $$$$$$12,
-    "site/sections/Category/CategoryGrid.tsx": $$$$$$13,
-    "site/sections/Category/CategoryList.tsx": $$$$$$14,
-    "site/sections/CategoryList.tsx": $$$$$$11,
+    "site/sections/Category/CategoryBanner.tsx": $$$$$$11,
+    "site/sections/Category/CategoryGrid.tsx": $$$$$$12,
+    "site/sections/Category/CategoryList.tsx": $$$$$$13,
+    "site/sections/CategoryList.tsx": $$$$$$14,
     "site/sections/ChoiceUltrafeu.tsx": $$$$$$15,
     "site/sections/Content/Benefits.tsx": $$$$$$16,
     "site/sections/Content/BlogPosts.tsx": $$$$$$17,
