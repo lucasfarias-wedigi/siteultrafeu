0d11582a4e5e56ab4385edc8f07ee584f498132f
Ricardo Henrique Gonzaga <ricardo.gonzaga@agenciam3.com>
2024-07-19 11:47:14 -0300

    feat: add bannertext section

---
+++ sections/BannerText.tsx
@@ -0,0 +1,46 @@
+import type { ImageWidget } from "apps/admin/widgets.ts";
+import Image from "apps/website/components/Image.tsx";
+import Breadcrumb from "../components/ui/Breadcrumb.tsx";
+import { useUI } from "../sdk/useUI.ts";
+
+interface Props {
+    imageDesktop?: ImageWidget;
+    imageMobile?: ImageWidget;
+    alt?: string;
+    /** @format html */
+    text?: string;
+}
+
+function BannerText({ imageDesktop, imageMobile, alt, text }: Props) {
+
+    const { showSearchResultBreadcrumb } = useUI();
+
+    showSearchResultBreadcrumb.value = false;
+
+    return (
+        <div className="w-full flex flex-col gap-4 pb-[20px] relative">
+            <div className="absolute top-0 left-[16px] text-white"> <Breadcrumb itemListElement={[]} /></div>
+            <Image
+                className="w-full hidden md:block"
+                src={imageDesktop}
+                alt={alt}
+                width={1440}
+                height={217}
+                loading="lazy"
+            />
+
+            <Image
+                className="w-full md:hidden"
+                src={imageMobile}
+                alt={alt}
+                width={1440}
+                height={171}
+                loading="lazy"
+            />
+
+            {text && <span class="mx-auto max-w-7xl px-4 xl:px-0" dangerouslySetInnerHTML={{ __html: text }}></span>}
+        </div>
+    )
+}
+
+export default BannerText
